d464125cf3ffcf557fdd762671a33645fed9620a
Muhammad Akbar <m.akbar6@outlook.com>
2022-07-30 22:02:54 +0500

    Update database.php

--- BackEnd/shared/config/database.php
+++ BackEnd/shared/config/database.php
@@ -3,7 +3,7 @@ class Database{
   
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "api_db";
+    private $db_name = "azainorg_by ";
     private $username = "root";
     private $password = "";
     public $conn;
